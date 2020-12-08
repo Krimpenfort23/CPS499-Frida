6c1e2c48892aaf3d9511974f5b25cd847df2181d
apple <evankrimp@gmail.com>
2020-12-07 23:30:21 -0500

    ShoppingCart.c and ShoppingCart.h started and work with the tcp connection

---
+++ ShoppingCart.h
@@ -0,0 +1,15 @@
+/**
+ * ShoppingCart.h
+ * @author: Evan Krimpenfort
+ *          Anna Duricy
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
